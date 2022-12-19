8c709b7bb6ebd223a846096927e89930df6b8f92
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-19 20:07:27 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_TECNO-CG7.mk
+    $(LOCAL_DIR)/twrp_CG7.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_TECNO-CG7-user \
-    omni_TECNO-CG7-userdebug \
-    omni_TECNO-CG7-eng
+    twrp_CG7-user \
+    twrp_CG7-userdebug \
+    twrp_CG7-eng
